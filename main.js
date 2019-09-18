b9b956ecd5c1c147f915bab9330d63514eba51c1
Isaiah Carter <izay.carter0509@gmail.com>
2019-09-18 19:41:21 -0400

    starting with one attack instead of two

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ $(function() {
         constructor(options){
             options = options || {};
             this.name = options.name;
-            this.mainAttack = {name:options, power: options};
-            this.secondAttack = {name:options, power: options};
+            this.Attack = {name:options, power: options};
             let defaults = {health:100};
             Object.assign(this,defaults,options)
         }
     };
 
-let joe = new Player({name:"joe",mainAttack:{name:"hammerTime", Power:10},secondAttack:{name:"sinister-snap", power: 5}});
+let joe = new Player({name:"joe",Attack:{name:"hammerTime", Power:10}});
 console.log(joe);
 
 });
