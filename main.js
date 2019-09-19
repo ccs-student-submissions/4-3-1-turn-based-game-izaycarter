2553ecc9abede809ce94e1d059fed768a777ea20
Isaiah Carter <izay.carter0509@gmail.com>
2019-09-19 18:48:07 -0400

    added bars still need to activate bars

--- main.js
+++ main.js
@@ -1,60 +1,60 @@
 $(function() {
 
 
-const startButton = $(".start-btn");
-const startPage = $("#start-page");
-const arena = $("#arena-page")
-
-    class Player{
-        constructor(options){
-            options = options || {};
-            this.name = options.name;
-            this.Attack = {name:options, power: options};
-            let defaults = {health:100};
-            Object.assign(this,defaults,options);
+    const startButton = $(".start-btn");
+    const startPage = $("#start-page");
+    const arena = $("#arena-page")
+    const yourHealthBar = $('#yourHealthBar');
+    const compHealthBar = $('#compHealthBar');
+    const attackButton = $('#attack');
+    let getstarted = arena.hide();
+        class Player{
+            constructor(options){
+                options = options || {};
+                this.name = options.name;
+                this.Attack = {name:options, power: options};
+                let defaults = {health:100};
+                Object.assign(this,defaults,options);
+            }
         }
-    }
 
 
-// hero stats
-let joe = new Player({name:"Joe",Attack:{name:"staff of destiny", Power:10}});
-joe.hero = true;
+    // hero stats
+    let joe = new Player({name:"Joe",Attack:{name:"staff of destiny", Power:10}});
+    joe.hero = true;
 
-let linda = new Player({name:"Linda",Attack:{name:"beam of intensity", Power:10}});
-linda.hero = true;
+    let linda = new Player({name:"Linda",Attack:{name:"beam of intensity", Power:10}});
+    linda.hero = true;
 
-let freddy = new Player({name:"Freddy",Attack:{name:"bolt of might", Power:10}});
-freddy.hero = true;
+    let freddy = new Player({name:"Freddy",Attack:{name:"bolt of might", Power:10}});
+    freddy.hero = true;
 
-let isaiah = new Player({name:"Isaiah",Attack:{name:"a literal monkeywrench", Power:10}});
-isaiah.hero = true;
-
-
-
-
-// villian stats
-
-let caleb = new Player({name:"caleb",Attack:{name:"swarm of talking ducks", Power:10}});
-caleb.hero = false;
-
-let rob = new Player({name:"Rob",Attack:{name:"sincopated music notes", Power:10}});
-rob.hero = false;
-
-let adien = new Player({name:"Adien",Attack:{name:"thumb tack shutgun", Power:12}});
-adien.hero = false;
-
-let will = new Player({name:"Will",Attack:{name:"evil yorkie", Power:10}});
-will.hero = false;
+    let isaiah = new Player({name:"Isaiah",Attack:{name:"a literal monkeywrench", Power:10}});
+    isaiah.hero = true;
 
 
 
-startButton.on("click", () => {
-    startPage.hide();
-    arena.Show();
-});
- let playtime = () => {
 
- };
+    // villian stats
 
+    let caleb = new Player({name:"caleb",Attack:{name:"swarm of talking ducks", Power:10}});
+    caleb.hero = false;
+
+    let rob = new Player({name:"Rob",Attack:{name:"sincopated music notes", Power:10}});
+    rob.hero = false;
+
+    let adien = new Player({name:"Adien",Attack:{name:"thumb tack shutgun", Power:12}});
+    adien.hero = false;
+
+    let will = new Player({name:"Will",Attack:{name:"evil yorkie", Power:10}});
+    will.hero = false;
+
+
+    startButton.on("click", () => {
+        startPage.hide();
+        if(getstarted === arena.hide()){
+            getstarted = arena.show()
+        }
+    });
 
 });
