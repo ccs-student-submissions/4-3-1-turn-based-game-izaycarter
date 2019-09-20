e93ef1c87d08cdae840c8438d7189c2e89cb1b3e
Isaiah Carter <izay.carter0509@gmail.com>
2019-09-20 17:28:35 -0400

    added photo switch back

--- main.js
+++ main.js
@@ -14,91 +14,92 @@ $(function() {
 
 
 
-    let yourattack = Math.round(Math.random()*15);
-    let compattack = Math.round(Math.random()*15);
+    let yourattack = Math.round(Math.random() * 15);
+    let compattack = Math.round(Math.random() * 15);
     let yourHealth = 100;
     let compHealth = 100;
+    let characters = [];
 
 
 
 
-
-        class Player{
-            constructor(options){
-                options = options || {};
-                this.name = options.name;
-            }
+    class Player {
+        constructor(options) {
+            options = options || {};
+            this.name = options.name;
         }
+    }
 
 
     // hero stats
     let Charhero = [{
 
-    name:'Joe',
-    Attack:10,
-    Power:85,
-    imageUrl:"https://media.giphy.com/media/zwhknCyqHExnW/giphy.gif",
-    hero: true
-  },{
-    name:'Linda',
-    Attack:10,
-    Power:85,
-    imageUrl:"https://media.giphy.com/media/11jokITGudhl8Q/source.gif",
-    hero: true
-  },{
-    name:'Freddy',
-    Attack:10,
-    Power:85,
-    imageUrl:"https://tenor.com/view/vine-crackhead-fight-gif-11019923",
-  },{
-    name:'Isaiah',
-    Attack:10,
-    Power:85,
-    imageUrl:"https://media.giphy.com/media/Exyy0Ocz88jQY/giphy.gif",
-    hero: true
-  },];
+        name: 'Joe',
+        Attack: 10,
+        Power: 85,
+        imageUrl: "https://media.giphy.com/media/zwhknCyqHExnW/giphy.gif",
+        hero: true
+    }, {
+        name: 'Linda',
+        Attack: 10,
+        Power: 85,
+        imageUrl: "https://media.giphy.com/media/11jokITGudhl8Q/source.gif",
+        hero: true
+    }, {
+        name: 'Freddy',
+        Attack: 10,
+        Power: 85,
+        imageUrl: "https://tenor.com/view/vine-crackhead-fight-gif-11019923",
+    }, {
+        name: 'Isaiah',
+        Attack: 10,
+        Power: 85,
+        imageUrl: "https://media.giphy.com/media/Exyy0Ocz88jQY/giphy.gif",
+        hero: true
+    }, ];
 
     // villian stats
 
-    let charVillian =[{
+    let charVillian = [{
 
-    name: 'caleb',
-    Attack:10,
-    Power:85,
-    imageUrl:"https://media.giphy.com/media/xiYMaJQ3XxxnHDqw8Q/giphy.gif",
-    hero: false
-},{
-    name: 'will',
-    Attack:10,
-    Power:85,
-    imageUrl:"https://media.giphy.com/media/3o6nUWPstbNkVjHB4Y/giphy.gif",
-    hero: false
-  },{
-    name: 'adien',
-    Attack:10,
-    Power:85,
-    imageUrl:"https://media.giphy.com/media/fGI7OX410yk0uGbZ1H/giphy.gif",
-    hero: false
-  },{
-    name: 'robbie',
-    Attack:10,
-    Power:85,
-    imageUrl:"https://media.giphy.com/media/8JNj02eQD4fPD2hR6d/giphy.gif",
-    hero: false
-  },];
+        name: 'caleb',
+        Attack: 10,
+        Power: 85,
+        imageUrl: "https://media.giphy.com/media/xiYMaJQ3XxxnHDqw8Q/giphy.gif",
+        hero: false
+    }, {
+        name: 'will',
+        Attack: 10,
+        Power: 85,
+        imageUrl: "https://media.giphy.com/media/3o6nUWPstbNkVjHB4Y/giphy.gif",
+        hero: false
+    }, {
+        name: 'adien',
+        Attack: 10,
+        Power: 85,
+        imageUrl: "https://media.giphy.com/media/fGI7OX410yk0uGbZ1H/giphy.gif",
+        hero: false
+    }, {
+        name: 'robbie',
+        Attack: 10,
+        Power: 85,
+        imageUrl: "https://media.giphy.com/media/8JNj02eQD4fPD2hR6d/giphy.gif",
+        hero: false
+    }, ];
 
 
     startButton.on("click", () => {
         startPage.hide();
-        if(getstarted === arena.hide()){
+        if (getstarted === arena.hide()) {
             getstarted = arena.show();
-        }
-        characters.forEach(function(character) {
-    game[character.name.toUpperCase()] = new Player(character);
-  });
+            }
+        $('#homepage').css("background-image", "url(https://ae01.alicdn.com/kf/HTB1DpBjknmWBKNjSZFBq6xxUFXaZ/Allenjoy-backgrounds-for-photography-studio-boxing-arena-blurred-Cheering-spectator-stadium-spotlight-backdrop-new-photocall.jpg_640x640q70.jpg)"); 
     });
 
-
+    // find out what this does,
+    // characters.forEach(function(character) {
+    //     game[character.name.toUpperCase()] = new Player(character);
+    // });
 
     attackButton.on("click", () => {
         compwidth = (compHealth -= yourattack) + "%";
@@ -107,14 +108,14 @@ $(function() {
         yourwidth = (yourHealth -= compattack) + "%";
         yourHealthBar.width(yourwidth);
 
-        if(compHealth <= 0 || yourHealth <= 0) {
+        if (compHealth <= 0 || yourHealth <= 0) {
             gameover();
         }
 
     });
 
-    let gameover = () =>{
-        if(compHealth <= 0){
+    let gameover = () => {
+        if (compHealth <= 0) {
             console.log('you win');
         } else {
             console.log('you lose');
